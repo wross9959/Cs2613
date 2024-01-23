5f3beb5a2d6b432259134b283c0d52a8251e292f
Will Ross <93093979+wross9959@users.noreply.github.com>
2024-01-23 19:09:43 -0400

    Add files via upload

---
+++ Octave/Labs/Lab1/Task1.m
@@ -0,0 +1,38 @@
+vector = [2, 0 , 3];
+
+function  n = unitVector(vector)
+    vectorToReturn = [];
+    bottomToSqrt = 0;
+    orinatation = iscolumn(vector);
+    numOfZero = 0;
+
+    if(orinatation == 1)
+        vector = transpose(vector);
+    else
+        for i = 1: length(vector)
+
+            bottomToSqrt = (bottomToSqrt + (vector(i))^2);
+
+            if(vector(i) == 0)
+
+                numOfZero = numOfZero + 1;
+
+            endif
+        end
+
+        if(numOfZero == length(vector))
+
+            n = vector;
+
+        endif
+        bottom = sqrt(bottomToSqrt);
+
+        for i = 1: length(vector) 
+            vectorToReturn(i) = vector(i)/bottom;
+        end
+
+        n = vectorToReturn;
+    end
+end
+
+display(unitVector(vector));
