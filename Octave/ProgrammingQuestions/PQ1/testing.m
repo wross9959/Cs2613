93efaaa06f9397e7ea1b99f5e49cf076b5f24086
Will Ross <wross9959@gmail.com>
2024-01-25 09:19:00 -0400

    Finished Octave

--- Octave/ProgrammingQuestions/PQ1/testing.m
+++ Octave/ProgrammingQuestions/PQ1/testing.m
@@ -1,3 +1,7 @@
+% @author Will Ross #3734692
+% Assesment: Octave PQ1 used for PQ4
+% Date: Jan 25th 2024
+% Class: Cs2613
 clear; clc
 [city, x_city, y_city] = textread('Cities.csv',...
              '%s %f %f',...
@@ -10,27 +14,19 @@ clear; clc
              'headerlines',1);
 
 
-% Displaying the city results // testing
-%for i = 1:length(city)
-%    fprintf('%s: (%f, %f)\n', city{i}, x(i), y(i));
-%end
-% Displaying the location results // testing
-%for i = 1:length(locationNum)
-%    fprintf('%d: (%f, %f)\n', locationNum(i), x2(i), y2(i));
-%end
-numCities = length(city);
-numLocations = length(locationNum);
-closestCounts = zeros(numCities, 1);
-nearestLocation = zeros(numCities, 1);
-nearestDistance = inf(numCities, 1);
+numOfCities = length(city);
+numOfLocations = length(locationNum);
+closestCities = zeros(numOfCities, 1);
+nearestLocation = zeros(numOfCities, 1);
+nearestDistance = inf(numOfCities, 1);
 
-for i = 1:numLocations
+for i = 1:numOfLocations
     locX = x_loc(i);
     locY = y_loc(i);
     closestCity = 0;
     minDist = inf;
 
-    for j = 1:numCities
+    for j = 1:numOfCities
         dist = sqrt((locX - x_city(j))^2 + (locY - y_city(j))^2);
         if dist < minDist
             minDist = dist;
@@ -38,7 +34,7 @@ for i = 1:numLocations
         end
     end
 
-    closestCounts(closestCity) += 1;
+    closestCities(closestCity) = closestCities(closestCity) + 1;
     if minDist < nearestDistance(closestCity)
         nearestDistance(closestCity) = minDist;
         nearestLocation(closestCity) = locationNum(i);
@@ -46,9 +42,9 @@ for i = 1:numLocations
 end
 
 
-
+%for loop to print all the values
 fprintf('| City | # Closest | Nearest Location\n');
 fprintf('===================================\n');
-for i = 1:numCities
-    fprintf('%s \t| %d \t| %d\n', city{i}, closestCounts(i), nearestLocation(i));
+for i = 1:numOfCities
+    fprintf('%s \t| %d \t| %d\n', city{i}, closestCities(i), nearestLocation(i));
 end
