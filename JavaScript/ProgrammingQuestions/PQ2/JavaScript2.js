90e223d7314818c5a221b4570d107bafdaf1f0dc
Will Ross <wross9959@gmail.com>
2024-01-28 21:44:39 -0400

    Updates

--- JavaScript/ProgrammingQuestions/PQ2/JavaScript2.js
+++ JavaScript/ProgrammingQuestions/PQ2/JavaScript2.js
@@ -82,271 +82,222 @@ parameter err that returns an error in case of any writing error.
 
 
 //let varibles for the functions
-let fileToAppend = 'C:\\Users\\willr\\Documents\\GitHub\\Cs2613\\JavaScript\\ProgrammingQuestions\\PQ2\\DataOutput.txt'
 const fileToRead = 'C:\\Users\\willr\\Documents\\GitHub\\Cs2613\\JavaScript\\ProgrammingQuestions\\PQ2\\DataInput.txt'
-var operators
-var values
-var numberOfValues
 const fs = require('fs');
 const file = fs.readFileSync(fileToRead, { encoding: 'utf8', flag: 'r' }).split("\n");
 
-
-
-
-//lists for all stored values
-
-//Operators in data
-operators = []
-
-//Number of values per operation
-numberOfValues = []
-
-//Values read in
-values = []
+//three arrays filled with the data
+const {operators, numberOfValues, values} = storeValues(file)
 
 //clears any data on the output file
-//clearFile(fileToAppend)
+clearFile()
 
-//throws all the files data into file varible
-//file = File(fileToRead, 'r').readlines()
-
-//three lists filled with the data
-operators, numberOfValues, values = storeValues(file)
-
-for(i in operators){
-    console.log(numberOfValues[i])
-}
 //gets the current operator
-//getCurrentOperation()
+getCurrentOperation(operators, numberOfValues, values)
 
 //prints finished message with filePath with results
 //print("Results were printed to \n\tfilePath: " + fileToAppend +"\n")
 
     
-// function clearFile(pathToNewFile){
-//     //clears the file to get ready to send new data
-//     open(pathToNewFile, 'w').close()
-// }
-// //change or remove
-// function openFile(){
-//     filePath = filedialog.askopenfilename(initialdir="C:\\Users\\willr\\Documents\\GitHub\\Cs2613\\Python\\ProgrammingQuestions\\PQ2",
-//                                           title="File to Read?",
-//                                           filetypes=(("text files","*.txt"),
-//                                                      ("all files", "*.*")))
-//     //gets the file needed for input with data
-//     return filePath
-// }
-// //change or remove
-// function openFileToWrite(){
-//     filePathOut = filedialog.askopenfilename(initialdir="C:\\Users\\willr\\Documents\\GitHub\\Cs2613\\Python\\ProgrammingQuestions\\PQ2",
-//                                           title="File to write to?",
-//                                           filetypes=(("text files","*.txt"),
-//                                                      ("all files", "*.*")))
-//     //gets the filePath for the file needed
-//     return filePathOut
-// }
-// //change
-// function File(inputFile, func){
-//     //reads/writes the file inputted
-//     input = open(inputFile, func)
-//     return input
-// }
+function clearFile(){
+    const fileToAppend = 'C:\\Users\\willr\\Documents\\GitHub\\Cs2613\\JavaScript\\ProgrammingQuestions\\PQ2\\DataOutput.txt'
+    const fs = require('fs');
+    fs.writeFileSync(fileToAppend, '');
+}
+
 
 
 
 function storeValues(file){
+    //Operators in data
+    let operators = []
+    //Values read in
+    let values = []
+    //amount of values 
+    let numberOfValues = []
 
-    for(i in file){
-
+    for(let i = 0; i < file.length; i++){
+        if(file[i] === "END"){
+            break
+        }
         //checks for commands
-        if(isLetter(i)){
-            operators.push(i)
-            console.log(i)
+        
+        let currOperator = (file[i])
+        operators.push(currOperator.trim())
+        //grab the amount of nums per operator
+        let num = parseInt(file[++i])
+        numberOfValues.push(num)
+        
+        for(let j = 0; j < num; j++){
+
+            //creating a curr varible cuase when trying to push on the same line it was causing the floats to become undefined
+            let curr = (parseFloat(file[j + 1 + i]))
+            values.push(curr)
         }
-        //checks for length of number of values
-        else if(Number.isInteger(i)){
-            numberOfValues.push(parseInt(i))
-            console.log(i)
-        }
-        //gathers all the values needed    
-        else if(isfloat(i)){
-            values.push(parseFloat(i))
-            console.log(i)
+        i += num
+    }
+    return {operators, numberOfValues, values}
+}
+function SUM(inputs){
+    let result = 0
+
+    for(i in inputs){
+        result += parseFloat(i)
+    }
+    return result
+}
+function AVG(inputs){
+    let result = 0
+
+    result = SUM(inputs) / inputs.length
+    return result
+}
+function MAX(inputs){
+    let result = 0
+    result = inputs[0]
+
+    for(i in inputs){
+        if(result < i){
+            result = i
         }
     }
-    return operators, numberOfValues, values
+    return result
 }
-function isLetter(str){
-    str = str.toUpperCase();
-    return str.length === 1 && str.match(/[A-Z]/i);
-}
-function isfloat(num){
-    //created this function to check if it was a float for input values
-    try{
-        parseFloat(num)
-        return true
+function MIN(inputs){
+    let result = 0
+    result = inputs[0]
+    
+    for(i in inputs){
+        if(result > i){
+            result = i
+        }
     }
-    catch (error){
-        return false
+    return result
+}
+function FXP(inputs){
+    let result = []
+    for(z in inputs){
+        let curr = []
+        for(let k = 0; k < 50; k++){
+            curr.push((z**k) /factorial(k))
+        }
+        result.push(SUM(curr))
+    }
+    return result
+}
+function FPO(inputs){
+    let result = []
+    
+    for(z in inputs){
+        let curr = []
+        for(let k = 0; k < 50; k++){
+            curr.push((k * ((z**k)/factorial(k))))
+        }
+        result.push(SUM(curr))
+    }
+    return result
+}
+function FSN(inputs){
+    let result = []
+    for(z in inputs){
+        let curr = []
+        for(let k = 0; k < 50; k++){
+            curr.push((((-1)**k) * ((z)**((2*k) + 1)))/(factorial((2*k) + 1)))
+        }
+        result.push(SUM(curr))
+    }
+    return result
+}
+function FCS(inputs){
+    let result = []
+    for(z in inputs){
+        let curr = []
+        for(let k = 0; k < 50; k++){
+            curr.push((((-1)**k) * ((z)**(2*k)))/(factorial(2*k)))
+        }
+        result.push(SUM(curr))
+    }
+    return result
+}
+function factorial(input){
+    if(input <= 1){
+        return 1
+    }
+    else{
+        return input * factorial(input -1)
     }
 }
-// function SUM(inputs){
-//     result = 0
 
-//     for i in inputs{
-//         result += i
-//     }
-//     return result
-// }
-// function AVG(inputs){
-//     result = 0
+function toWrite(input, command){
+    const fileToAppend = 'C:\\Users\\willr\\Documents\\GitHub\\Cs2613\\JavaScript\\ProgrammingQuestions\\PQ2\\DataOutput.txt'
+    const fs = require('fs')
+    let dataToWrite = (command + " Results:\n")
 
-//     result = SUM(inputs) / len(inputs)
-//     return result
-// }
-// function MAX(inputs){
-//     result = 0
-//     result = inputs[0]
+    if(Array.isArray(input)){
+        for(let i = 0; i < input.length; i++){
+            //writes the list values for our F functions
+            dataToWrite += ((i+1) + ":\t" + (input[i]) + "\n")
+        }
+    }
+    else{
+        //writes the single value function results
+        dataToWrite += ("#:\t" + (input) + "\n")
+    }
+    fs.appendFileSync(fileToAppend, dataToWrite)
+}
 
-//     for i in inputs{
-//         if(result < i){
-//             result = i
-//         }
-//     }
-//     return result
-// }
-// function MIN(inputs){
-//     result = 0
-//     result = inputs[0]
-    
-//     for i in inputs{
-//         if(result > i){
-//             result = i
-//         }
-//     }
-//     return result
-// }
-
-// function FXP(inputs){
-//     result = []
-//     for z in inputs{
-//         curr = []
-//         for k in range(50){
-//             curr.append((z**k) / math.factorial(k))
-//         }
-//         result.append(SUM(curr))
-//     }
-//     return result
-// }
-
-// function FPO(inputs){
-//     result = []
-    
-//     for z in inputs{
-//         curr = []
-//         for k in range(50){
-//             curr.append((k * ((z**k)/math.factorial(k))))
-//         }
-//         result.append(SUM(curr))
-//     }
-//     return result
-// }
-
-// function FSN(inputs){
-//     result = []
-//     for z in inputs{
-//         curr = []
-//         for k in range(50){
-//             curr.append((((-1)**k) * ((z)**((2*k) + 1)))/(math.factorial((2*k) + 1)))
-//         }
-//         result.append(SUM(curr))
-//     }
-//     return result
-// }
-
-// function FCS(inputs){
-//     result = []
-//     for z in inputs{
-//         curr = []
-//         for k in range(50){
-//             curr.append((((-1)**k) * ((z)**(2*k)))/(math.factorial(2*k)))
-//         }
-//         result.append(SUM(curr))
-//     }
-//     return result
-// }
-
-// function toString(input, command){
-//     let fileToAppend
-//     f = File(fileToAppend, 'a')
-
-//     f.write(command + " Results:\n")
-
-//     if(type(input) == list){
-//         for i in range(len(input)){
-//             //writes the list values for our F functions
-//             f.write(str(i+1) + ":\t" + str(input[i]) + "\n")
-//         }
-//     }
-//     else{
-//         //writes the single value function results
-//         f.write("#:\t" + str(input) + "\n")
-//     }
-//     f.close()
-// }
-
-// function CommandSwitch(command,inputs){
-//     //checks which operator 
-//     switch(command){
-//         case "SUM":
-//             toString(SUM(inputs), command)
+function CommandSwitch(command,inputs){
+    //checks which operator 
+    command = command.trim()
+    switch(command){
+        case "SUM":
+            toWrite(SUM(inputs), command)
+            break
+        case "AVG":
+            toWrite(AVG(inputs), command)
+            break
+        case "MAX":
+            toWrite(MAX(inputs), command)
+            break
+        case "MIN":
+            toWrite(MIN(inputs), command)
+            break
+        case "FXP":
+            toWrite(FXP(inputs), command)
+            break
+        case "FPO":
+            toWrite(FPO(inputs), command)
+            break
+        case "FSN":
+            toWrite(FSN(inputs), command)
+            break
+        case "FCS":
+            toWrite(FCS(inputs), command)
+            break
+        //incase an error happens 
+        default:
+            console.log("No function found")
+            break
         
-//         case "AVG":
-//             toString(AVG(inputs), command)
-        
-//         case "MAX":
-//             toString(MAX(inputs), command)
-        
-//         case "MIN":
-//             toString(MIN(inputs), command)
-        
-//         case "FXP":
-//             toString(FXP(inputs), command)
-        
-//         case "FPO":
-//             toString(FPO(inputs), command)
-        
-//         case "FSN":
-//             toString(FSN(inputs), command)
-        
-//         case "FCS":
-//             toString(FCS(inputs), command)
-        
-//         //incase an error happens 
-//         default:
-//             print("No function found")
-        
-//     }
-// }  
+    }
+}  
 
-// function getCurrentOperation(){
-//     //used to pop the End operator off the list 
-//     operators.pop()
+function getCurrentOperation(operators, numberOfValues, values){
+    //start is equal to the first value in values
+    let start = 0
+    let end = 0
+    for(let i = 0; i < operators.length; i++){
+        //used to get the elements needed in the values list
+        end = start + numberOfValues[i]
+        //switch to check the operator and what values are needed per operator
+        CommandSwitch(operators[i], (values.slice(start, end)))
 
-//     //start is equal to the first value in values
-//     start = 0
-    
-//     for i in range (len(operators)){
+        //sets the starting elemets to the end of the list value for the next operator
+        start = end
+    }
         
-//         //used to get the elements needed in the values list
-//         end = start + numberOfValues[i]
+        
+}
 
-//         //switch to check the operator and what values are needed per operator
-//         (CommandSwitch(operators[i], (values[start:end])))
-
-//         //sets the starting elemets to the end of the list value for the next operator
-//         start = end
-//     }
-// }
 
 
