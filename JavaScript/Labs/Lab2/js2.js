f149204003838f952c95d8cf4cc4b115dfe134b6
Will Ross <wross9959@gmail.com>
2024-02-01 23:00:00 -0400

    almost

--- JavaScript/Labs/Lab2/js2.js
+++ JavaScript/Labs/Lab2/js2.js
@@ -1,50 +1,60 @@
 var bestPerformers = [];
 var bestPerformersValues = [];
 var pplCount = [0.0, 0.0, 0.0];
-var ppl = ["Abby", "Tyson", "Zack"];
+var pplCountIndex = 0;
+var startFilePath = 'C:\\Users\\willr\\Documents\\GitHub\\Cs2613\\JavaScript\\Labs\\Lab2\\'
+// var filePath = 'C:\\Users\\willr\\Documents\\GitHub\\Cs2613\\JavaScript\\Labs\\Lab2\\calculations1.json'
 
-function readJSON(file) {
-    const fs = require('node:fs');
-    let data = fs.readFileSync(file, 'utf8');
+let accuracy = { "Abby": 0, "Tyson": 0, "Zack": 0 };
 
-    let curr = JSON.parse(data).data.calculations;
-    let trueValue = parseFloat(curr[0].calc);
+const fs = require('node:fs');
 
-    let lowestDifference = Number.MAX_VALUE;
-    let bestPerformerIndex = -1;
+let index = 1;
+try{
+    while(true){
+        let filePath = `${startFilePath}calculations${index}.json`;
+        if (!fs.existsSync(filePath)) break; // Check if file exists to avoid unnecessary error
 
-    // Calculate differences for Abby, Tyson, and Zack
-    for (let i = 1; i <= 3; i++) {
-        let current = parseFloat(curr[i].calc);
+        let data = fs.readFileSync(filePath, 'utf8');
+        let curr = JSON.parse(data).data.calculations;
+        calculate(curr)
+        index++
+    }
+
+}catch(err){
+    console.error("Error processing files:", err.message);
+}
+
+
+function calculate(file){
+
+    let closest = Infinity;
+    let trueValue = parseFloat(file[0].calc);
+    let fileBestPerformer = "";
+
+    for(let i = 1; i < file.length; i++){
+        let current = parseFloat(file[i].calc);
         let difference = Math.abs(trueValue - current);
-        
-        if (difference < lowestDifference) {
-            lowestDifference = difference;
-            bestPerformerIndex = i - 1;
+        pplCount[i] += (difference)
+
+        if(difference < closest){ 
+            fileBestPerformer = file[i].performer
+            closest = difference;
+            
+            pplCountIndex = i;
         }
-
-        pplCount[i - 1] += difference; // Accumulate differences
     }
-
-    bestPerformers.push(ppl[bestPerformerIndex]);
-    bestPerformersValues.push(pplCount[bestPerformerIndex]);
+    bestPerformers.push(fileBestPerformer)
 }
+function toPrint(){
+    for(let i = 1; i < bestPerformers.length; i++){
+        console.log(`${i}. Name: ${bestPerformers[i]} \tValue: ${pplCount[pplCountIndex]}`)
 
-var cont = true;
-for (let i = 1; cont; i++) {
-    try {
-        let file = `calculations${i}.json`;
-        readJSON(file);
-    } catch (err) {
-        cont = false;
-        console.log("Finished reading files.");
     }
-}
+    console.log(`${1}. Name: ${bestPerformers[0]} \tValue: ${pplCount[pplCountIndex]}`)
+    // console.log(bestPerformers[0])
+    // console.log(pplCount[pplCountIndex])
 
-function toPrint() {
-    for (let i = 0; i < bestPerformers.length; i++) {
-        console.log(`${i + 1}:\t ${bestPerformers[i]}\t (${bestPerformersValues[i]})`);
-    }
 }
 
 toPrint();
