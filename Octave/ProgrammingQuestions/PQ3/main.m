cf342cc2f5b3e6a93a2432b0756c196667c4b24b
Will Ross <wross9959@gmail.com>
2024-03-05 14:17:52 -0400

    updates

--- Octave/ProgrammingQuestions/PQ3/main.m
+++ Octave/ProgrammingQuestions/PQ3/main.m
@@ -1,59 +1,63 @@
 disp("");
-freqTableBeowulf = createFrequencyTable('Beowulf.txt');
-freqTableVindication = createFrequencyTable('Vindication.txt');
-determineOrigin(freqTableBeowulf, freqTableVindication, "This is the message");
 
-function freqTable = createFrequencyTable(filePath)
-  % Read the entire file into a string
-  fid = fopen(filePath, 'r');
-  text = fscanf(fid, '%c');
-  fclose(fid);
-  
-  % Tokenize the text
-  tokens = strsplit(text);
-  
-  % Create a frequency table
-  uniqueTokens = unique(tokens);
-  freqTable = struct();
-  for i = 1:length(uniqueTokens)
-    token = uniqueTokens{i};
-    freqTable.(token) = sum(strcmp(tokens, token));
-  end
-end
+function retval = createFrequencyTable(fileName)
 
-function determineOrigin(freqTable1, freqTable2, text)
-  tokens = strsplit(text);
-  totalTokens1 = sum(structfun(@(x) x, freqTable1));
-  totalTokens2 = sum(structfun(@(x) x, freqTable2));
-  
-  logProb1 = 0;
-  logProb2 = 0;
-  
-  for i = 1:length(tokens)
-    token = tokens{i};
-    
-    % Calculate the probability of each token in each text
-    if isfield(freqTable1, token)
-      logProb1 += log((freqTable1.(token) + 1) / (totalTokens1 + length(fieldnames(freqTable1))));
-    else
-      logProb1 += log(1 / (totalTokens1 + length(fieldnames(freqTable1))));
-    end
-    
-    if isfield(freqTable2, token)
-      logProb2 += log((freqTable2.(token) + 1) / (totalTokens2 + length(fieldnames(freqTable2))));
-    else
-      logProb2 += log(1 / (totalTokens2 + length(fieldnames(freqTable2))));
-    end
-  end
-  
-  % Determine the more likely source
-  if logProb1 > logProb2
-    disp('More likely written by the author of Beowulf');
-  elseif logProb2 > logProb1
-    disp('More likely written by Mary Wollstonecraft');
-  else
-    disp('Could be from either author.');
-  end
-end
+	% File name
+	data = fileread(fileName);
+	
+	% Removing White Space
+	tokens = strsplit(data);
+	
+	%get unique tokens in the input file
+	uniqueTokens = unique(tokens);
+	
+	%create the basic structure to return with values
+	retval = struct();
+	
+	for i = 1:length(uniqueTokens)
+		%gets the amount of time each token is repeated and plugs into the freq table
+		curr = uniqueTokens{i};
+		retval.(curr) = sum(strcmp(tokens, curr));
+	end;
+end;
 
 
+function retval = naiveBayes(dictionary, token)
+	tokenLength = length(dictionary.(token));
+	% checks if there is a row by the name of the token to check
+	if isfield(dictionary, token)
+	  retval += log((dictionary.(token) + 1) / (tokenLength) + length(fieldnames(dictionary)));
+	else
+	  retval += log(1 / (totalTokens + length(fieldnames(dictionary))));
+	end;
+end;
+
+function retval = determineOrigin(freqTable1, freqTable2, word)
+
+	tokens = strsplit(word);
+	
+	for i = 1:length(tokens)
+		token = tokens{i};
+	end;
+	%gets the probability of eachs freq table
+	logProb1 = naiveBayes(freqTable1, token);
+	logProb2 = naiveBayes(freqTable2, token);
+
+	% If more word tokens in Beowulf which is freqTable1
+	if logProb1 > logProb2
+		disp('More likely written by the author of Beowulf');
+		
+	% If more word tokens in Wollstonecraft which is freqTable2
+  	elseif logProb2 > logProb1
+		disp('More likely written by Mary Wollstonecraft');
+	% If there equal or both zero
+  	else
+		disp('Could be from either author.');
+  	end;
+end;
+
+dictionary1 = createFrequencyTable("Beowulf.txt");
+dictionary2 = createFrequencyTable("Vindication.txt");
+% checkInput = input("What is the message you would like to check: ");
+
+determineOrigin(dictionary1, dictionary2, "This is the message");
